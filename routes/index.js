aa8cc1d641417cb63ca07be6b48edb6fde7c0e3e
petii7 <dgb280799@gmail.com>
2025-12-03 13:32:05 +0100

    Enhance application structure and styling
    - Updated CSS in public/stylesheets/style.css to improve layout and responsiveness.
    - Modified routes/index.js to include new routes for displaying Pokémon and their types.
    - Added user login functionality in routes/users.js with session management.
    - Enhanced views/index.ejs to dynamically display registered Pokémon with improved HTML structure.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const dataService = require('../data/dataService');
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let pokemons = dataService.findAllPokemons();
+  let tipos = dataService.findAllPokemonTypes();
+  res.render('index', { pokemons, tipos });
+});
+
+router.get("/pokemon/:pid",function(req,res,next){
+  let pokemon = dataService.findPokemonById(req.params.pid);
+  let tipos = dataService.findAllPokemonTypes();
+  res.render('pokemon', { pokemon, tipos })
+});
+
+router.get('/tipos/:tipo', (req, res, next) => {
+  const tipo = req.params.tipo;
+  const pokemons = dataService.findAllPokemonsByType(tipo);
+  const tipos = dataService.findAllPokemonTypes();
+  res.render('index', { pokemons, tipos });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post("/login", function(req, res, next) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (dataService.validateUser(username, password)) {
+    req.session.isLogged = true;
+    req.session.username = username;
+
+    res.redirect("/");
+  } else {
+    res.render("login", { error: "User does not exist" });
+  }
+});
+
 module.exports = router;
